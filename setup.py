a68c6b67119041974e2139bc5062304d080889c9
rahesh <66882038+rahesh808@users.noreply.github.com>
2024-10-29 16:48:00 +0530

    Adding all the codes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI code analysis Project',
+    version= '0.0.0',
+    author= 'Aman Singh',
+    author_email= 'demo123@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
